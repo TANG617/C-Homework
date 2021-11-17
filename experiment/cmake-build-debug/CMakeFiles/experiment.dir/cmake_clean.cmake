c06343e2cec10a98cc8eac5026da7668b82427fa
LiTang <litang0617@outlook.com>
2021-11-17 11:47:22 +0800

    test project

---
+++ experiment/cmake-build-debug/CMakeFiles/experiment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/experiment.dir/main.c.o"
+  "experiment"
+  "experiment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/experiment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
